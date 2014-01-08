07b0d23d7d48bdf90c72d13624377e97c54194f9
Luke Lewandowski <luke@lewandowskis.com>
2014-01-08 18:24:41 +1100

    Very simple Windows Forms application that takes file to open and then spits out formatted file.

---
+++ BrokenJsonFormatter.Win/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BrokenJsonFormatter.Win
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileLabel = new System.Windows.Forms.Label();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.FormatAndSaveLabel = new System.Windows.Forms.Label();
+            this.formatAndSaveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenFileLabel
+            // 
+            this.OpenFileLabel.AutoSize = true;
+            this.OpenFileLabel.Location = new System.Drawing.Point(47, 38);
+            this.OpenFileLabel.Name = "OpenFileLabel";
+            this.OpenFileLabel.Size = new System.Drawing.Size(176, 13);
+            this.OpenFileLabel.TabIndex = 0;
+            this.OpenFileLabel.Text = "1. Select broken JSON file to format";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(50, 64);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(173, 23);
+            this.openFileBtn.TabIndex = 1;
+            this.openFileBtn.Text = "Open File";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // FormatAndSaveLabel
+            // 
+            this.FormatAndSaveLabel.AutoSize = true;
+            this.FormatAndSaveLabel.Location = new System.Drawing.Point(47, 108);
+            this.FormatAndSaveLabel.Name = "FormatAndSaveLabel";
+            this.FormatAndSaveLabel.Size = new System.Drawing.Size(100, 13);
+            this.FormatAndSaveLabel.TabIndex = 2;
+            this.FormatAndSaveLabel.Text = "2. Format and Save";
+            // 
+            // formatAndSaveBtn
+            // 
+            this.formatAndSaveBtn.Location = new System.Drawing.Point(50, 134);
+            this.formatAndSaveBtn.Name = "formatAndSaveBtn";
+            this.formatAndSaveBtn.Size = new System.Drawing.Size(173, 23);
+            this.formatAndSaveBtn.TabIndex = 3;
+            this.formatAndSaveBtn.Text = "Format and Save";
+            this.formatAndSaveBtn.UseVisualStyleBackColor = true;
+            this.formatAndSaveBtn.Click += new System.EventHandler(this.formatAndSaveBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.formatAndSaveBtn);
+            this.Controls.Add(this.FormatAndSaveLabel);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.OpenFileLabel);
+            this.Name = "MainForm";
+            this.Text = "Broken Json Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OpenFileLabel;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.Label FormatAndSaveLabel;
+        private System.Windows.Forms.Button formatAndSaveBtn;
+
+    }
+}
+
---
+++ BrokenJsonFormatter.Win/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrokenJsonFormatter.Win
+{
+    public partial class MainForm : Form
+    {
+        private string filePath;
+        private StringBuilder outputString = new StringBuilder();
+
+        private int _currentDepth;
+        private int currentDepth
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    _currentDepth = value;
+                }
+            }
+
+            get
+            {
+                return _currentDepth;
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Open broken JSON file";
+            openFileDialog.Filter = "JSON Documents (.json)|*.json";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    filePath = openFileDialog.FileName;
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to open file.");
+                }
+            }
+        }
+
+        private void formatAndSaveBtn_Click(object sender, EventArgs e)
+        {
+            if (filePath != null && !string.IsNullOrEmpty(filePath))
+            {
+                var inputFile = File.ReadAllText(filePath);
+                currentDepth = 0;
+                var soFar = new StringBuilder();
+
+                for (int i = 0; i < inputFile.Length; i++)
+                {
+                    if (inputFile[i] == '{' || inputFile[i] == '[')
+                    {
+                        
+
+                        if (soFar.Length > 0)
+                        {
+                            outputString.AppendLine(paddingWithDepth(soFar.ToString(), currentDepth));
+                            soFar.Remove(0, soFar.Length);
+                        }
+
+                        outputString.AppendLine(paddingWithDepth(inputFile[i].ToString(), currentDepth));
+
+                        currentDepth++;
+                    }
+                    else if (inputFile[i] == '}' || inputFile[i] == ']')
+                    {
+                        if (soFar.Length > 0)
+                        {
+                            outputString.AppendLine(paddingWithDepth(soFar.ToString(), currentDepth));
+                            soFar.Remove(0, soFar.Length);
+                        }
+
+                        currentDepth--;
+
+                        outputString.AppendLine(paddingWithDepth(inputFile[i].ToString(), currentDepth));
+                    }
+                    else if (inputFile[i] == ',')
+                    {
+                        soFar.Append(inputFile[i]);
+                        soFar.Append(Environment.NewLine);
+                        soFar.Append(paddingWithDepth(string.Empty, currentDepth));
+                    }
+                    else
+                    {
+                        soFar.Append(inputFile[i]);
+                    }
+                }
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.Title = "Save formatted broken JSON file";
+                saveFileDialog.Filter = "JSON Documents (.json)|*.json";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, outputString.ToString());
+                }
+            }else{
+                MessageBox.Show("Please select broken file to open first.");
+            }
+        }
+
+        private string paddingWithDepth(string characterSet, int depth)
+        {
+            var padding = new StringBuilder();
+
+            for (int i = 0; i < depth; i++)
+            {
+                padding.Append('\t');
+            }
+
+            if (!string.IsNullOrEmpty(characterSet))
+            {
+                padding.Append(characterSet);
+            }
+            
+            return padding.ToString();
+        }
+    }
+}
